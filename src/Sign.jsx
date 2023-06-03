29b2485d9e1bacaa9648a6e53f5ebc1a0849264d
mariyanixon <134275856+mariyanixon@users.noreply.github.com>
2023-06-03 19:37:15 +0530

    Update Sign.jsx

--- src/Sign.jsx
+++ src/Sign.jsx
@@ -4,19 +4,44 @@ import './Sign.css';
 
 const Sign = () => {
   const [showTerms, setShowTerms] = useState(false);
+  const [resetClicked, setResetClicked] = useState(false);
+  const [name, setName] = useState('');
+  const [place, setPlace] = useState('');
+  const [age, setAge] = useState('');
+  const [email, setEmail] = useState('');
+  const [education, setEducation] = useState('');
+  const [dob, setDob] = useState('');
+  const [phone, setPhone] = useState('');
+  const [contact, setContact] = useState('');
 
   useEffect(() => {
-    if (showTerms) {
+    if (showTerms && !resetClicked) {
       const timeout = setTimeout(() => {
         setShowTerms(false);
-      }, 3000);
+      }, 5000);
 
       return () => clearTimeout(timeout);
     }
-  }, [showTerms]);
+  }, [showTerms, resetClicked]);
 
   const handleShowTerms = () => {
     setShowTerms(true);
+  };
+
+  const handleReset = () => {
+    setShowTerms(false);
+    setResetClicked(true);
+    setName('');
+    setPlace('');
+    setAge('');
+    setEmail('');
+    setEducation('');
+    setDob('');
+    setPhone('');
+    setContact('');
+    setTimeout(() => {
+      setResetClicked(false);
+    }, 5000);
   };
 
   return (
@@ -33,35 +58,35 @@ const Sign = () => {
           <h2>Sign Up</h2>
           <div style={{ display: 'flex', gap: '50px' }}>
             <p>Name</p>
-            <TextField variant="filled" type="text" />
+            <TextField variant="filled" type="text" value={name} onChange={(e) => setName(e.target.value)} />
           </div>
           <div style={{ display: 'flex', gap: '50px' }}>
             <p>Place</p>
-            <TextField variant="filled" type="text" />
+            <TextField variant="filled" type="text" value={place} onChange={(e) => setPlace(e.target.value)} />
           </div>
           <div style={{ display: 'flex', gap: '50px' }}>
             <p>Age</p>
-            <TextField variant="filled" type="number" />
+            <TextField variant="filled" type="number" value={age} onChange={(e) => setAge(e.target.value)} />
           </div>
           <div style={{ display: 'flex', gap: '50px' }}>
             <p>Email id</p>
-            <TextField variant="filled" type="email" />
+            <TextField variant="filled" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
           </div>
           <div style={{ display: 'flex', gap: '50px' }}>
             <p>Education</p>
-            <TextField variant="filled" type="text" />
+            <TextField variant="filled" type="text" value={education} onChange={(e) => setEducation(e.target.value)} />
           </div>
           <div style={{ display: 'flex', gap: '50px' }}>
             <p>Date of Birth</p>
-            <TextField variant="filled" type="date" />
+            <TextField variant="filled" type="date" value={dob} onChange={(e) => setDob(e.target.value)} />
           </div>
           <div style={{ display: 'flex', gap: '50px' }}>
             <p>Phone no.</p>
-            <TextField variant="filled" type="number" />
+            <TextField variant="filled" type="number" value={phone} onChange={(e) => setPhone(e.target.value)} />
           </div>
           <div style={{ display: 'flex', gap: '50px' }}>
             <p>Contact Details</p>
-            <TextField variant="filled" type="text" />
+            <TextField variant="filled" type="text" value={contact} onChange={(e) => setContact(e.target.value)} />
           </div>
           <br />
           <br />
@@ -69,7 +94,7 @@ const Sign = () => {
             <Button variant="contained" color="success">
               Submit
             </Button>
-            <Button variant="contained" color="warning">
+            <Button variant="contained" color="warning" onClick={handleReset}>
               Reset
             </Button>
             <Button variant="contained" color="secondary" onClick={handleShowTerms}>
